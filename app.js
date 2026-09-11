87623fc76069a98ef161c86c495ba17defb13cbe
TsheyRing <07240120.sherubtse@rub.edu.bt>
2026-09-11 15:53:45 +0600

    Unit IV Lab 3: PostgreSQL connection, Restaurant and MenuItem models, restaurant-specific menus

--- app.js
+++ app.js
@@ -23,3 +23,8 @@ app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
 });
 
+const db = require('./config/db');
+app.get('/db-test', async (req, res) => {
+ const result = await db.one('SELECT NOW() AS current_time');
+ res.json(result);
+});
